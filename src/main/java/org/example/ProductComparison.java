125fb697ac3316019e25b06e0b605f289c25d1ef
tripti <triptilakhina9286@gmail.com>
2023-04-23 01:20:21 +0100

    Selenium first homework commit

--- src/main/java/org/example/ProductComparison.java
+++ src/main/java/org/example/ProductComparison.java
@@ -23,39 +23,44 @@ public class ProductComparison {
         driver.findElement(By.xpath("(//button[@class=\"button-2 add-to-compare-list-button\"])[3]")).click();
 
         // Add gift card - product 2 to add to compare
-  //      driver.findElement(By.xpath("(//button[@class=\"button-2 add-to-compare-list-button\"])[4]")).click();
+ //       driver.findElement(By.xpath("(//button[@class=\"button-2 add-to-compare-list-button\"])[4]")).click();
 
         // link text for notification bar
   //     driver.findElement(By.linkText("product comparison")).click();
 
 
-
         // link text from footer
   //     driver.findElement(By.linkText("Compare products list")).click();
 
-        //
-  //      driver.findElement(By.xpath("(//a[@href=\"/compareproducts\"])[2]")).click();
-
-        driver.findElement(By.xpath("(//a[@href=\"/compareproducts\"])[1]")).click();
-
-//
 
         //xpath from footer when no notification tab is displayed
-  //      driver.findElement(By.xpath("//a[@href=\"/compareproducts\"]")).click();
+  //     driver.findElement(By.xpath("//a[@href=\"/compareproducts\"]")).click();
 
 
         // Xpath from footer when notification bar is displayed
- //       driver.findElement(By.xpath("(//a[@href=\"/compareproducts\"])[2]")).click();
+        driver.findElement(By.xpath("(//a[@href=\"/compareproducts\"])[2]")).click();
 
-        //Xpath from notification bar is displayed
-  //      driver.findElement(By.xpath("(//a[@href=\"/compareproducts\"])[1]")).click();
 
-        String item2 = driver.findElement(By.linkText("$25 Virtual Gift Card")).getText();
-        System.out.println("Item 2 product name is:" + item2);
+        //Xpath from notification bar when it is displayed
+ //      driver.findElement(By.xpath("(//a[@href=\"/compareproducts\"])[1]")).click();
+
 
         String item1 = driver.findElement(By.xpath("(//a[@href=\"/htc-one-m8-android-l-50-lollipop\"])[2]")).getText();
-        System.out.println("Item 1 product name is:" + item1);
+        System.out.println("Item 1 product name is: " + item1);
 
+//        String item2 = driver.findElement(By.xpath("(//a[@href=\"/25-virtual-gift-card\"])[2]")).getText();
+//        System.out.println("Item 2 product name is: " + item2);
+
+        // Click on clear list
+        driver.findElement(By.className("clear-list")).click();
+
+
+        // Print error message
+        String noItemsToCompare = driver.findElement(By.className("no-data")).getText();
+        System.out.println("Message displayed to user on clearing the compare list: " + noItemsToCompare);
+
+
+        driver.close();
 
 
 
